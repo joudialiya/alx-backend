8b2eb8bd3ab90f53bbcfbbf03c1a5e00589b920e
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-29 17:55:16 +0000

    0-4.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic cache context class"""
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO cache context class"""
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        keys = list(self.cache_data.keys())
+        # if the key already exists
+        # we update it
+        if key in keys:
+            self.cache_data[key] = item
+            return
+        # else we add a new one, taking into account
+        # the MAX_ITEMS
+        if len(self.cache_data) < self.MAX_ITEMS:
+            self.cache_data[key] = item
+            return
+
+        print("DISCARD: {}".format(keys[0]))
+        self.cache_data.pop(keys[0])
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
